e8116319556bda0c008938607f9248efc658a66a
ashutosh150195 <ashutosh150195@gmail.com>
2016-11-23 17:04:39 +0530

    Finally mission Accomplished

---
+++ StammeringCure/app/src/main/java/com/example/think_twice_code_once/stammeringcure/UserRegistration.java
@@ -0,0 +1,17 @@
+package com.example.think_twice_code_once.stammeringcure;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by think-twice-code-once on 23/11/16.
+ */
+
+public class UserRegistration extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_registration);
+    }
+}
